02a3c76b888a12e9054951cd6260e7d4d71e1f74
TheReakFakeAdmin <therealfakedev@gmail.com>
2024-10-13 03:48:36 +0000

    /v2/spacex/timeline fixed with overhaul
    - Returns an array with SpaceX api data, and time as "T-/+HH:MM:SS"
    - given a t0 (ex: 2024-10-13T07:00-0500) adds the timestamp (ts) in milliseconds for each step (if timelines in hh:mm:ss format)

--- routes-v2/spacex/timeline.js
+++ routes-v2/spacex/timeline.js
@@ -1,12 +1,69 @@
 const getMission = require('./modules/get_mission'),
 
-timeline = async function (req, res) {
+toUnixTimestamp = (time) => {
+    const hhmmss = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
+
+    if (!hhmmss.test(time)) return null;
+
+    const [hours, minutes, seconds] = time.split(":").map(Number);
+
+    const hoursInMilliseconds = hours * 60 * 60 * 1000;
+    const minutesInMilliseconds = minutes * 60 * 1000;
+    const secondsInMilliseconds = seconds * 1000;
+
+    return hoursInMilliseconds + minutesInMilliseconds + secondsInMilliseconds;
+},
+
+timeline = async (req, res) => {
     const missionId = req.params["missionId"],
-    mission = await getMission(missionId),
-    ary = [];
+        t0 = new Date(req.query["t0"]),
+        t0Bool = !isNaN(t0),
+        resp = await getMission(missionId),
+        mission = resp.data,
+        ary = [];
+
+    if (resp.error !== null) return res.send(resp);
 
     let preLaunchTimeline = mission?.preLaunchTimeline?.timelineEntries,
     postLaunchTimeline = mission?.postLaunchTimeline?.timelineEntries;
+
+    const hhmmss = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
+
+    if (hhmmss.test(preLaunchTimeline[0].time)) {
+        for (let i in preLaunchTimeline) {
+            let time = preLaunchTimeline[i].time,
+                isTime = hhmmss.test(preLaunchTimeline[i].time);
+
+            switch (t0Bool && isTime) {
+                case true:
+                    const t = t0.getTime() - toUnixTimestamp(time);
+                    preLaunchTimeline[i].ts = t;
+                    preLaunchTimeline[i].iso = new Date(t).toISOString();
+                    break;
+                case false:
+                    break;
+            }
+            preLaunchTimeline[i].time = `T-${preLaunchTimeline[i].time}`;
+        }
+    }
+
+    if (hhmmss.test(postLaunchTimeline[0].time)) {
+        for (let i in postLaunchTimeline) {
+            let time = postLaunchTimeline[i].time,
+                isTime = hhmmss.test(postLaunchTimeline[i].time);
+
+            switch (t0Bool && isTime) {
+                case true:
+                    const t = t0.getTime() + toUnixTimestamp(time);
+                    postLaunchTimeline[i].ts = t;
+                    postLaunchTimeline[i].iso = new Date(t).toISOString();
+                    break;
+                case false:
+                    break;
+            }
+            postLaunchTimeline[i].time = `T+${postLaunchTimeline[i].time}`;
+        }
+    }
 
     if (Array.isArray(preLaunchTimeline))
         ary.push(...preLaunchTimeline);
@@ -26,6 +83,6 @@ timeline = async function (req, res) {
     })
 
     res.send(ary);
-}
+};
 
 module.exports = timeline;
